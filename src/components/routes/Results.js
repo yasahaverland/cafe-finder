571533b040caa32a83f27231b929fc367f5995b9
brandon-w1205 <bwu1205@gmail.com>
2022-10-10 15:10:03 -0700

    adds prop passability

--- src/components/routes/Results.js
+++ src/components/routes/Results.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Result } from '../routes/Result'
 
-export default function Results() {
-
+export default function Results(props) {
+	console.log(props.results)
+	const cafe = props.results.map(result => {
+		return (
+			<div>
+				<h2>{result.name}</h2>
+			</div>
+		)
+	}) 
 
 	return (
 		<div>
+			{cafe}
 			<Result 
 			/>
 		</div>
